f1d008cdaba45566eb31ec7cd7d8f19327b08ca8
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2022-04-12 16:22:23 -0400

    Manipulating CSS Styles

--- script.js
+++ script.js
@@ -21,10 +21,20 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+  // When there is no input
+
   if (!guess) {
     document.querySelector(".message").textContent = "No number!";
+
+    // When players win
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number!😍";
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too high";
@@ -34,6 +44,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "U lost the game!";
       document.querySelector(".score").textContent = 0;
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too low";
