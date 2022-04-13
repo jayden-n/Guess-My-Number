7b0829c6ead3dbeecee4839fbbdf2196ec2a2a63
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2022-04-13 10:18:54 -0400

    Implementing Highscores

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
+let highScore = 0;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
@@ -33,6 +35,14 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
 
     document.querySelector(".number").style.width = "30rem";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    } else if (score < highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
